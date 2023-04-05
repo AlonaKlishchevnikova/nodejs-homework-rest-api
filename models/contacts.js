1d4b0746f4498342f9f1f0a766889dd7e16373f7
AlenaKiporuk <alenakiporyk@gmail.com>
2023-04-05 11:31:56 +0300

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,13 +1,14 @@
 const fs = require("fs/promises");
 const { nanoid } = require("nanoid");
 const path = require("path");
-const contactsPath = path.join(__dirname, "../models/contacts.json");
+  const contactsPath = path.join(__dirname, "../models/contacts.json");
 
 
 const listContacts= async ()=> {
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
 }
+
 const getContactById = async (id)=> {
     const contacts = await listContacts(id);
     const result = contacts.find(item => item.id === id);
@@ -39,10 +40,21 @@ const addContact = async (name, email, phone)=> {
     return newContact;
 
 }
+const updateById = async (id, data) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(item => item.id === id);
+    if(index === -1){
+        return null;
+    }
+    contacts[index] = {id, ...data};
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return contacts[index];
+}
 
 module.exports = {
     listContacts,
     getContactById,
     addContact,
-    removeContact
+  removeContact,
+    updateById
 }
